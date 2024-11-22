acb15f8a737e1ed5e6164c5384cf3f05c7770cb1
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-11-22 19:19:12 +0530

    error reducer added in slice.

--- src/slices/form-slice.jsx
+++ src/slices/form-slice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   firstName: null,
   lastName: null,
-  erros: {},
+  errors: {},
 };
 
 const userSlice = createSlice({
@@ -16,9 +16,12 @@ const userSlice = createSlice({
     setLastName: (state, action) => {
       state.lastName = action.payload;
     },
+    setErrors: (state, action) => {
+      state.errors = action.payload;
+    },
   },
 });
 
-export const { setFirstName, setLastName } = userSlice.actions;
+export const { setFirstName, setLastName, setErrors } = userSlice.actions;
 
 export default userSlice.reducer;
