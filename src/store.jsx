f5d3282e146f3e0805952335ec6e64e8b1d772b7
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-11-24 12:16:22 +0530

    slice created for prove main component

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './slices/form-slice';
+import ProveMain from "./Components/ProveMain";
 
 
 const store = configureStore({
     reducer: {
-        userSliceInfo: userSlice
+        userSliceInfo: userSlice,
+        ProveMainInfo: ProveMain
     }
 });
 
